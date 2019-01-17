8f1db9cf377662a059022a35096b7805ad62c2e5
Vijay <vijay@vijaydhyani2196@gmail.com>
2019-01-17 13:33:23 +0530

    first commit

---
+++ src/main/java/com/vijay/hellofromrestfulservice/HellofromrestfulserviceApplication.java
@@ -0,0 +1,14 @@
+package com.vijay.hellofromrestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellofromrestfulserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellofromrestfulserviceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/vijay/hellofromrestfulservice/HellofromrestfulserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.vijay.hellofromrestfulservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HellofromrestfulserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
